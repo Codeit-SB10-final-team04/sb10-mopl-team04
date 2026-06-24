2ad663a21cb98c646360b63a205c886fb91c44ca
LeeDyol <dlekthf819203@naver.com>
2026-06-24 11:34:30 +0900

    feat: 팔로우 생성 API 컨트롤러 구현

---
+++ src/main/java/com/team04/mopl/follow/controller/FollowController.java
@@ -0,0 +1,35 @@
+package com.team04.mopl.follow.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.team04.mopl.follow.dto.request.FollowRequest;
+import com.team04.mopl.follow.dto.response.FollowDto;
+import com.team04.mopl.follow.service.FollowService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/follows")
+public class FollowController implements FollowControllerDocs {
+
+	private final FollowService followService;
+
+	@Override
+	@PostMapping
+	public ResponseEntity<FollowDto> createFollow(
+		@Valid @RequestBody FollowRequest followRequest/*t,
+		TODO: 메서드를 요청한 사용자 정보가 필요 -> 임시로 CustomUserDetails로 구현하되, 주석 처리함
+		@AuthenticationPrincipal CustomUserDetails userDetails*/
+	) {
+		FollowDto response = followService.createFollow(followRequest/*, userDetails*/);
+
+		return ResponseEntity.status(HttpStatus.CREATED).body(response);
+	}
+}
