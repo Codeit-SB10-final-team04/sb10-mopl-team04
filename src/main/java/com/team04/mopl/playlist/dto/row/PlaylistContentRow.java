7699d1b5f65c6862e5f879f026bbd83e1dca8131
JungH200000 <jhpaxk0604@gmail.com>
2026-06-24 12:31:18 +0900

    chore: 설명 주석 추가

--- src/main/java/com/team04/mopl/playlist/dto/row/PlaylistContentRow.java
+++ src/main/java/com/team04/mopl/playlist/dto/row/PlaylistContentRow.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 
 import com.team04.mopl.content.entity.Content;
 
+// 플레이리스트 id별 콘텐츠를 일괄 조회하기 위한 Repository 조회용 DTO
 public record PlaylistContentRow(
 
 	UUID playlistId,
--- src/main/java/com/team04/mopl/playlist/dto/row/PlaylistSubscriberCountRow.java
+++ src/main/java/com/team04/mopl/playlist/dto/row/PlaylistSubscriberCountRow.java
@@ -2,6 +2,7 @@ package com.team04.mopl.playlist.dto.row;
 
 import java.util.UUID;
 
+// 플레이리스트 id별 구독자 수를 일괄 조회하기 위한 Repository 조회용 DTO
 public record PlaylistSubscriberCountRow(
 
 	UUID playlistId,
