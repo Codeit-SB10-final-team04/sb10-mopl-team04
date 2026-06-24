443ea9a141abe4ad44639fb6a38bc3f27ebb701b
JungH200000 <jhpaxk0604@gmail.com>
2026-06-24 13:22:01 +0900

    feat: 콘텐츠 id별 태그명 일괄 조회 repository 메서드 추가

--- src/main/java/com/team04/mopl/content/repository/ContentTagRepository.java
+++ src/main/java/com/team04/mopl/content/repository/ContentTagRepository.java
@@ -1,10 +1,22 @@
 package com.team04.mopl.content.repository;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import com.team04.mopl.content.dto.row.TagRow;
 import com.team04.mopl.content.entity.ContentTag;
 
 public interface ContentTagRepository extends JpaRepository<ContentTag, UUID> {
+
+	@Query(value = """
+		SELECT new com.team04.mopl.content.dto.row.TagRow(ct.content.id, ct.tag.name)
+		FROM ContentTag AS ct
+		LEFT JOIN ct.tag AS t
+		WHERE ct.content.id IN :contentIds
+		""")
+	List<TagRow> findTagNamesByContentIds(@Param("contentIds") List<UUID> contentIds);
 }
