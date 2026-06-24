e9459b2a733004e634f9c1ac3fa57d23f199430f
LeeDyol <dlekthf819203@naver.com>
2026-06-24 11:49:17 +0900

    feat: 팔로우 도메인 MapStruct 매퍼 구현

---
+++ src/main/java/com/team04/mopl/follow/mapper/FollowMapper.java
@@ -0,0 +1,23 @@
+package com.team04.mopl.follow.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.MapperConfig;
+import org.mapstruct.Mapping;
+
+import com.team04.mopl.follow.dto.response.FollowDto;
+import com.team04.mopl.follow.entity.Follow;
+
+@Mapper(config = MapperConfig.class)
+public interface FollowMapper {
+
+	// FollowRequest -> Follow Entity
+	@Mapping(target = "followee", source = "followee")
+	@Mapping(target = "follower", source = "follower")
+	// Follow toEntity(User followee, User follower);
+
+	// Follow Entity -> FollowDto
+	@Mapping(target = "id", source = "follow.id")
+	@Mapping(target = "followeeId", source = "follow.followee.id")
+	@Mapping(target = "followerId", source = "follow.follower.id")
+	FollowDto toDto(Follow follow);
+}
