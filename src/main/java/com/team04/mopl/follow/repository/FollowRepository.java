225dadc8efa61a09dc63e90314b68d1b5914fc78
LeeDyol <dlekthf819203@naver.com>
2026-06-24 11:37:24 +0900

    feat: 팔로우 중복 검사를 위한 쿼리 메서드 구현

--- src/main/java/com/team04/mopl/follow/repository/FollowRepository.java
+++ src/main/java/com/team04/mopl/follow/repository/FollowRepository.java
@@ -9,4 +9,7 @@ import com.team04.mopl.follow.entity.Follow;
 
 @Repository
 public interface FollowRepository extends JpaRepository<Follow, UUID> {
+
+	// 유효성 검증: 중복 검사
+	boolean existsByFolloweeIdAndFollowerId(UUID followeeId, UUID followerId);
 }
