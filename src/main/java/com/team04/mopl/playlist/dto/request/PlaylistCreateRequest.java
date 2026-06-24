61354b10d1b7a60d57f058d16316768e75362f57
JungH200000 <jhpaxk0604@gmail.com>
2026-06-24 15:52:59 +0900

    refactor: 검증 메시지 추가

--- src/main/java/com/team04/mopl/playlist/dto/request/PlaylistCreateRequest.java
+++ src/main/java/com/team04/mopl/playlist/dto/request/PlaylistCreateRequest.java
@@ -5,12 +5,12 @@ import jakarta.validation.constraints.Size;
 
 public record PlaylistCreateRequest(
 
-	@NotBlank(message = "title이 입력되지 않았습니다.")
-	@Size(max = 100, min = 1)
+	@NotBlank(message = "플레이리스트 제목이 입력되지 않았습니다.")
+	@Size(min = 1, max = 100, message = "플레이리스트 제목은 1~100 글자 이상이어야 합니다.")
 	String title,
 
-	@NotBlank(message = "description이 입력되지 않았습니다.")
-	@Size(min = 1)
+	@NotBlank(message = "플레이리스트 설명이 입력되지 않았습니다.")
+	@Size(min = 1, message = "플레이리스트 설명은 1 글자 이상이어야 합니다.")
 	String description
 ) {
 }
