a248fddefc53913f1fb540cbe3cb021e60b37156
LeeDyol <dlekthf819203@naver.com>
2026-06-24 16:18:34 +0900

    feat: 팔로우 대상에 대한 팔로워 수 카운트 메서드 추가

--- src/main/java/com/team04/mopl/follow/service/FollowService.java
+++ src/main/java/com/team04/mopl/follow/service/FollowService.java
@@ -6,10 +6,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.team04.mopl.auth.repository.AuthSessionRepository;
+import com.team04.mopl.follow.dto.request.FollowRequest;
 import com.team04.mopl.follow.exception.FollowErrorCode;
 import com.team04.mopl.follow.exception.FollowException;
 import com.team04.mopl.follow.mapper.FollowMapper;
 import com.team04.mopl.follow.repository.FollowRepository;
+import com.team04.mopl.user.entity.User;
 import com.team04.mopl.user.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -51,6 +53,15 @@ public class FollowService {
 	// 	return followMapper.toDto(newFollow);
 	// }
 
+	// 특정 유저의 팔로우 수 조회
+	public Long getFollowerCount(FollowRequest followRequest) {
+		// 1. 유효성 검증: 특정 사용자 존재 확인
+		User targetUser = getUserEntityOrThrow(followRequest.followeeId());
+
+		// 2. 특정 사용자의 팔로우 수 조회 및 반환
+		return followRepository.countByFolloweeId(targetUser.getId());
+	}
+
 	// 유효성 검증: 팔로우 중복 검사 여부
 	private void validateDuplicateFollow(UUID followeeId, UUID followerId) {
 		if (followRepository.existsByFolloweeIdAndFollowerId(followeeId, followerId)) {
@@ -60,11 +71,12 @@ public class FollowService {
 		}
 	}
 
-	// // 엔티티 반환
-	// private User getUserEntityOrThrow(UUID userId) {
-	// 	return userRepository.findById(userId)
-	// 		.orElseThrow(() -> new Userxception(
-	// 			UserErrorCode
-	// 		));
-	// }
+	// 사용자 엔티티 반환
+	private User getUserEntityOrThrow(UUID userId) {
+		return userRepository.findById(userId)
+			// TODO: User 도메인의 최상위 예외 클래스 구현 시 주석 제거 예정
+			.orElseThrow(/*() -> new Userxception(
+				UserErrorCode
+			)*/);
+	}
 }
