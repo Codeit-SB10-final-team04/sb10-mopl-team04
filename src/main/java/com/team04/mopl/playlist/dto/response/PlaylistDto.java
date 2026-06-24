205e990f7b4280ddc7e0836d087768c17818e7c0
JungH200000 <jhpaxk0604@gmail.com>
2026-06-24 10:48:23 +0900

    feat: 응답 DTO인 `PlaylistDto` 생성

---
+++ src/main/java/com/team04/mopl/playlist/dto/response/PlaylistDto.java
@@ -0,0 +1,21 @@
+package com.team04.mopl.playlist.dto.response;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.UUID;
+
+import com.team04.mopl.content.dto.response.ContentSummary;
+import com.team04.mopl.user.dto.response.UserSummary;
+
+public record PlaylistDto(
+
+	UUID id,
+	UserSummary owner,
+	String title,
+	String description,
+	Instant updatedAt,
+	long subscriberCount,
+	boolean subscribedByMe,
+	List<ContentSummary> contents
+) {
+}
