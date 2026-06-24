81c662e1ea5708f5c6061fe1d2c4a70e75ddb244
JungH200000 <jhpaxk0604@gmail.com>
2026-06-24 19:46:51 +0900

    feat: 플레이리스트 단건 조회 API 구현

--- src/main/java/com/team04/mopl/playlist/controller/PlaylistController.java
+++ src/main/java/com/team04/mopl/playlist/controller/PlaylistController.java
@@ -4,6 +4,8 @@ import java.util.UUID;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -38,5 +40,21 @@ public class PlaylistController implements PlaylistControllerDocs {
 		PlaylistDto playlistDto = playlistService.createPlaylist(request, currentUserId);
 
 		return ResponseEntity.status(HttpStatus.CREATED).body(playlistDto);
+	}
+
+	@GetMapping(value = "/{playlistId}")
+	@Override
+	public ResponseEntity<PlaylistDto> findPlaylist(
+		@PathVariable UUID playlistId,
+		@RequestHeader("X-MOPL-USER-ID") UUID currentUserId
+		// @AuthenticationPrincipal MoplUserDetails moplUserDetails
+	) {
+		// TODO: Security 구현 완료 후 @AuthenticationPrincipal 사용
+		// Security 구현 완료 전까지 임시 헤더로 받기
+		// TODO: Security 구현 완료 후 주석 해제
+		// UUID currentUserId = moplUserDetails.getId();
+		PlaylistDto playlistDto = playlistService.findPlaylist(playlistId, currentUserId);
+
+		return ResponseEntity.status(HttpStatus.OK).body(playlistDto);
 	}
 }
