6e12b18e62a38737197f0f98883c263d58d4e787
LeeDyol <dlekthf819203@naver.com>
2026-06-24 11:25:31 +0900

    feat: Direct Message 도메인 에러 코드(ErrorCode) 정의

---
+++ src/main/java/com/team04/mopl/directmessage/exception/DirectMessageErrorCode.java
@@ -0,0 +1,31 @@
+package com.team04.mopl.directmessage.exception;
+
+import org.springframework.http.HttpStatus;
+
+import com.team04.mopl.common.exception.ErrorCode;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public enum DirectMessageErrorCode implements ErrorCode {
+	DM_BLANK(HttpStatus.BAD_REQUEST, "DM01", "메시지 내용은 공백일 수 없습니다.");
+
+	private final HttpStatus httpStatus;
+	private final String code;
+	private final String description;
+
+	@Override
+	public HttpStatus getHttpStatus() {
+		return this.httpStatus;
+	}
+
+	@Override
+	public String getCode() {
+		return this.code;
+	}
+
+	@Override
+	public String getMessage() {
+		return this.description;
+	}
+}
