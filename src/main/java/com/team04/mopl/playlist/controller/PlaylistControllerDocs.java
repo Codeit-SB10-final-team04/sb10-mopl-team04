0c4fb0346b2f1e70f35e89a374901d220985dcfe
JungH200000 <jhpaxk0604@gmail.com>
2026-06-24 15:35:17 +0900

    chore: `@Tag`의 `description` 오타 수정

--- src/main/java/com/team04/mopl/playlist/controller/PlaylistControllerDocs.java
+++ src/main/java/com/team04/mopl/playlist/controller/PlaylistControllerDocs.java
@@ -10,7 +10,7 @@ import com.team04.mopl.playlist.dto.response.PlaylistDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
-@Tag(name = "플레이리스트 관리", description = "플레이리스  API")
+@Tag(name = "플레이리스트 관리", description = "플레이리스트 API")
 public interface PlaylistControllerDocs {
 
 	@Operation(summary = "플레이리스트 등록", description = "생성한 플레이리스트는 API 요청자 본인의 플레이리스트로 생성됩니다.")
