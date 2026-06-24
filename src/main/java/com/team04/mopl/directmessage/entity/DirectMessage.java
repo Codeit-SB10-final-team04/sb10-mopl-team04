a9173ea6fee0a40838818fe196c5406d9245f0a5
LeeDyol <dlekthf819203@naver.com>
2026-06-24 10:43:39 +0900

    feat: 디엠 도메인 엔티티 및 읽음 상태 관리 로직 구현

---
+++ src/main/java/com/team04/mopl/directmessage/entity/DirectMessage.java
@@ -0,0 +1,80 @@
+package com.team04.mopl.directmessage.entity;
+
+import java.time.Instant;
+
+import com.team04.mopl.common.entity.BaseUpdatableEntity;
+import com.team04.mopl.conversation.entity.Conversation;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "direct_messages")
+public class DirectMessage extends BaseUpdatableEntity {
+
+	/*
+		TODO: User 생성 후 주석 제거 예정
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "sender_id", nullable = false)
+	private User sender;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "receiver_id", nullable = false)
+	private User receiver;
+	 */
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "conversation_id", nullable = false)
+	private Conversation conversation;
+
+	@Column(name = "content", nullable = false, columnDefinition = "TEXT")
+	private String content;
+
+	@Column(name = "is_read", nullable = false)
+	private boolean isRead;
+
+	@Column(name = "read_at")
+	private Instant readAt;
+
+	@Builder
+	public DirectMessage(
+		// User sender,
+		// User receiver,
+		Conversation conversation,
+		String content
+	) {
+		validateContent(content);
+
+		// this.sender = sender;
+		// this.receiver = receiver;
+		this.conversation = conversation;
+		this.content = content;
+		this.isRead = false;        // 기본 상태: 안 읽음
+		this.readAt = null;
+	}
+
+	public void updateIsRead(boolean isRead) {
+		this.isRead = isRead;
+
+		// 읽은 경우에만 갱신
+		if (isRead) {
+			this.readAt = Instant.now();
+		}
+	}
+
+	private void validateContent(String content) {
+		if (content == null || content.isBlank()) {
+			throw new DirectMessageException(DirectMessageErrorCode.DM_BLANK);
+		}
+	}
+}
