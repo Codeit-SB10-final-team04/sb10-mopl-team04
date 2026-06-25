e01ff5364968879b2633cea6fef0e05a5e0200a8
JungH200000 <jhpaxk0604@gmail.com>
2026-06-25 14:05:13 +0900

    feat: 수정 권한이 없을 경우의 커스텀 예외 코드 추가

--- src/main/java/com/team04/mopl/playlist/exception/PlaylistErrorCode.java
+++ src/main/java/com/team04/mopl/playlist/exception/PlaylistErrorCode.java
@@ -11,7 +11,8 @@ public enum PlaylistErrorCode implements ErrorCode {
 
 	INVALID_INPUT(HttpStatus.BAD_REQUEST, "PL01", "잘못된 입력값입니다."),
 	NO_CHANGE_VALUE(HttpStatus.BAD_REQUEST, "PL02", "변경사항이 없습니다."),
-	PLAYLIST_NOT_FOUND(HttpStatus.NOT_FOUND, "PL03", "플레이리스트를 찾을 수 없습니다.");
+	PLAYLIST_FORBIDDEN(HttpStatus.FORBIDDEN, "PL03", "플레이리스트 소유자가 아닙니다."),
+	PLAYLIST_NOT_FOUND(HttpStatus.NOT_FOUND, "PL04", "플레이리스트를 찾을 수 없습니다.");
 
 	private final HttpStatus httpStatus;
 	private final String code;
