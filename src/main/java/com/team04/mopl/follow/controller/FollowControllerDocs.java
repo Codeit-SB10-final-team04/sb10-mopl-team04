fc77fd242add0b873ce5ae5d350a9fdb37dde173
LeeDyol <dlekthf819203@naver.com>
2026-06-25 09:40:18 +0900

    docs: FollowController 스웨거 명세 추가

--- src/main/java/com/team04/mopl/follow/controller/FollowControllerDocs.java
+++ src/main/java/com/team04/mopl/follow/controller/FollowControllerDocs.java
@@ -4,11 +4,19 @@ import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "Follow API", description = "팔로우 관련 API")
 public interface FollowControllerDocs {
 	// 팔로우 생성
 	// TODO: 메서드를 요청한 사용자 정보가 필요 -> 임시로 CustomUserDetails로 구현하되, 주석 처리함
 	// ResponseEntity<FollowDto> createFollow(FollowRequest followRequest/*, CustomUserDetails userDetails */);
 
 	// 특정 사용자의 팔로우 수 조회
+	@Operation(
+		summary = "특정 사용자의 팔로우 수 조회",
+		description = "특정 사용자를 팔로우 하고 있는 사용자의 수를 반환합니다."
+	)
 	ResponseEntity<Long> getFollowerCount(UUID followeeId);
 }
