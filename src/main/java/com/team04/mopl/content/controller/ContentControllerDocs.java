d9750a69eb9642aa3fc65f61c02ca1ceb1a59741
jsj <sungju3115@naver.com>
2026-06-25 16:40:20 +0900

    docs: 메서드 설명 추가

--- src/main/java/com/team04/mopl/content/controller/ContentControllerDocs.java
+++ src/main/java/com/team04/mopl/content/controller/ContentControllerDocs.java
@@ -6,9 +6,11 @@ import org.springframework.http.ResponseEntity;
 
 import com.team04.mopl.content.dto.response.ContentDto;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @Tag(name = "Content API", description = "콘텐츠 관리")
 public interface ContentControllerDocs {
+	@Operation(summary = "콘텐트 단건 조회")
 	ResponseEntity<ContentDto> getContent(UUID contentId);
 }
