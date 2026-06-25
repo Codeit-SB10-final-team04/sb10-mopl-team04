505ed4eb84e2c6259f513c82a0b406949935e394
jsj <sungju3115@naver.com>
2026-06-25 13:36:08 +0900

    feat: 콘텐츠 에러 코드 추가

--- src/main/java/com/team04/mopl/content/exception/ContentErrorCode.java
+++ src/main/java/com/team04/mopl/content/exception/ContentErrorCode.java
@@ -1,4 +1,31 @@
 package com.team04.mopl.content.exception;
 
-public enum ContentErrorCode {
+import org.springframework.http.HttpStatus;
+
+import com.team04.mopl.common.exception.ErrorCode;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public enum ContentErrorCode implements ErrorCode {
+	CONTENT_NOT_FOUND(HttpStatus.NOT_FOUND, "CT01", "콘텐츠를 찾을 수 없습니다.");
+
+	private final HttpStatus httpStatus;
+	private final String code;
+	private final String description;
+
+	@Override
+	public HttpStatus getHttpStatus() {
+		return this.httpStatus;
+	}
+
+	@Override
+	public String getCode() {
+		return this.code;
+	}
+
+	@Override
+	public String getMessage() {
+		return this.description;
+	}
 }
