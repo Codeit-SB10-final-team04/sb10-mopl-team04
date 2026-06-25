8d7968213e98252b424b7937ade91beb57068323
parkngg <millco0803com@gmail.com>
2026-06-25 11:48:13 +0900

    refactor: mapper 공통 설정 적용

--- src/main/java/com/team04/mopl/user/mapper/UserMapper.java
+++ src/main/java/com/team04/mopl/user/mapper/UserMapper.java
@@ -2,10 +2,11 @@ package com.team04.mopl.user.mapper;
 
 import org.mapstruct.Mapper;
 
+import com.team04.mopl.config.MapStructConfig;
 import com.team04.mopl.user.dto.response.UserDto;
 import com.team04.mopl.user.entity.User;
 
-@Mapper(componentModel = "spring")
+@Mapper(config = MapStructConfig.class)
 public interface UserMapper {
 
 	UserDto toDto(User user);
