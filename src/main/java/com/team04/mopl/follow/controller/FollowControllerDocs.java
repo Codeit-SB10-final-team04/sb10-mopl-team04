c4c1dcb40e89a185606d71b08fd4a899d5c8c20f
LeeDyol <dlekthf819203@naver.com>
2026-06-25 14:43:02 +0900

    docs: 불필요한 주석 삭제

--- src/main/java/com/team04/mopl/follow/controller/FollowControllerDocs.java
+++ src/main/java/com/team04/mopl/follow/controller/FollowControllerDocs.java
@@ -13,7 +13,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 
 @Tag(name = "Follow API", description = "팔로우 관련 API")
 public interface FollowControllerDocs {
-	// 팔로우 생성
+
 	// TODO: 메서드를 요청한 사용자 정보가 필요 -> 임시로 CustomUserDetails로 구현하되, 주석 처리함
 	@Operation(
 		summary = "팔로우 생성",
@@ -25,7 +25,6 @@ public interface FollowControllerDocs {
 		// @AuthenticationPrincipal MoplUserDetails moplUserDetails
 	);
 
-	// 특정 사용자의 팔로우 수 조회
 	@Operation(
 		summary = "특정 사용자의 팔로우 수 조회",
 		description = "특정 사용자를 팔로우 하고 있는 사용자의 수를 반환합니다."
