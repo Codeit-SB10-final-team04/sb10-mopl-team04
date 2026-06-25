18f64a11dbefab4f281871326666b7dda8ff018b
LeeDyol <dlekthf819203@naver.com>
2026-06-25 10:20:58 +0900

    refactor: @NotNull 애노테이션 내 메시지 내용 수정

--- src/main/java/com/team04/mopl/follow/dto/request/FollowRequest.java
+++ src/main/java/com/team04/mopl/follow/dto/request/FollowRequest.java
@@ -5,7 +5,7 @@ import java.util.UUID;
 import jakarta.validation.constraints.NotNull;
 
 public record FollowRequest(
-	@NotNull(message = "팔로우 하고자 하는 사람의 ID는 필수값입니다.")
+	@NotNull(message = "팔로우 하고자 하는 사람의 ID를 입력해주세요.")
 	UUID followeeId
 ) {
 }
