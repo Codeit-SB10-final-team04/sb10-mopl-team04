8f37c5e8b4932c5c776478db5a2e8474e7f6c070
LeeDyol <dlekthf819203@naver.com>
2026-06-25 15:23:47 +0900

    feat: 팔로우 정보 미존재 예외 처리 코드 추가

--- src/main/java/com/team04/mopl/follow/exception/FollowErrorCode.java
+++ src/main/java/com/team04/mopl/follow/exception/FollowErrorCode.java
@@ -9,7 +9,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum FollowErrorCode implements ErrorCode {
 	FOLLOW_ALREADY(HttpStatus.CONFLICT, "FW01", "이미 해당 사용자를 팔로우 한 상태입니다."),
-	FOLLOW_SELF_NOT_ALLOWED(HttpStatus.BAD_REQUEST, "FW02", "본인을 팔로우 할 수 없습니다.");
+	FOLLOW_SELF_NOT_ALLOWED(HttpStatus.BAD_REQUEST, "FW02", "본인을 팔로우 할 수 없습니다."),
+	FOLLOW_NOT_FOUND(HttpStatus.NOT_FOUND, "FW03", "해당 팔로우의 정보를 찾을 수 없습니다.");
 
 	private final HttpStatus httpStatus;
 	private final String code;
