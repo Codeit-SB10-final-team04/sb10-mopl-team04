ae9449b39b8b8012fb18217a9664ab4450f4ba94
jsj <sungju3115@naver.com>
2026-06-25 13:31:00 +0900

    feat: 콘텐츠 커스텀 예외, 에러코드 구현

---
+++ src/main/java/com/team04/mopl/content/exception/ContentErrorCode.java
@@ -0,0 +1,4 @@
+package com.team04.mopl.content.exception;
+
+public enum ContentErrorCode {
+}
---
+++ src/main/java/com/team04/mopl/content/exception/ContentException.java
@@ -0,0 +1,23 @@
+package com.team04.mopl.content.exception;
+
+import java.util.Map;
+
+import com.team04.mopl.common.exception.ErrorCode;
+import com.team04.mopl.common.exception.MoplException;
+
+public class ContentException extends MoplException {
+
+	public ContentException(ErrorCode errorCode) {
+		super(errorCode);
+	}
+
+	public ContentException(ErrorCode errorCode, Map<String, Object> details) {
+		super(errorCode);
+
+		if (details == null) {
+			return;
+		}
+
+		details.forEach(this::addDetail);
+	}
+}
