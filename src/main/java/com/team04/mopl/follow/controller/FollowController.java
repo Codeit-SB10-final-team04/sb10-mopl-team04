18551faff8fedf26c0282ddbab4c44f9274db90b
LeeDyol <dlekthf819203@naver.com>
2026-06-25 13:27:06 +0900

    feat: 팔로우 API 엔드포인트 구현

--- src/main/java/com/team04/mopl/follow/controller/FollowController.java
+++ src/main/java/com/team04/mopl/follow/controller/FollowController.java
@@ -5,12 +5,18 @@ import java.util.UUID;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.team04.mopl.follow.dto.request.FollowRequest;
+import com.team04.mopl.follow.dto.response.FollowDto;
 import com.team04.mopl.follow.service.FollowService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -20,17 +26,21 @@ public class FollowController implements FollowControllerDocs {
 
 	private final FollowService followService;
 
-	// @Override
-	// @PostMapping
-	// public ResponseEntity<FollowDto> createFollow(
-	// 	@Valid @RequestBody FollowRequest followRequest/*t,
-	// 	TODO: 메서드를 요청한 사용자 정보가 필요 -> 임시로 CustomUserDetails로 구현하되, 주석 처리함
-	// 	@AuthenticationPrincipal MoplUserDetails moplUserDetails*/
-	// ) {
-	// 	FollowDto response = followService.createFollow(followRequest/*, userDetails*/);
-	//
-	// 	return ResponseEntity.status(HttpStatus.CREATED).body(response);
-	// }
+	@Override
+	@PostMapping
+	public ResponseEntity<FollowDto> createFollow(
+		@Valid @RequestBody FollowRequest followRequest,
+		@RequestHeader("X-MOPL-USER-ID") UUID currentUserId
+		// @AuthenticationPrincipal MoplUserDetails moplUserDetails
+	) {
+		// TODO: Security 구현 완료 후 @AuthenticationPrincipal 사용
+		// Security 구현 완료 전까지 임시 헤더로 받기
+		// UUID currentUserId = moplUserDetails.getId();
+
+		FollowDto followDto = followService.createFollow(followRequest, currentUserId);
+
+		return ResponseEntity.status(HttpStatus.CREATED).body(followDto);
+	}
 
 	@Override
 	@GetMapping("/count")
