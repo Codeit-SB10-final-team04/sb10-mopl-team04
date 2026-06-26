d22a25d4944f09aaaa137da49bb1a1e1e7c3789e
jsj <sungju3115@naver.com>
2026-06-26 11:19:12 +0900

    dto 쪽 검증 추가

--- src/main/java/com/team04/mopl/content/dto/request/ContentCreateRequest.java
+++ src/main/java/com/team04/mopl/content/dto/request/ContentCreateRequest.java
@@ -2,9 +2,14 @@ package com.team04.mopl.content.dto.request;
 
 import java.util.List;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record ContentCreateRequest(
+	@NotBlank(message = "콘텐츠 타입은 필수입니다.")
 	String type,
+	@NotBlank(message = "콘텐츠 제목은 필수입니다.")
 	String title,
+	@NotBlank(message = "콘텐츠 설명은 필수입니다.")
 	String descriptions,
 	List<String> tags
 ) {
---
+++ src/main/java/com/team04/mopl/content/storage/exception/FileStorageException.java
@@ -0,0 +1,7 @@
+package com.team04.mopl.content.storage.exception;
+
+public class FileStroageException extends RuntimeException {
+  public FileStroageException(String message) {
+    super(message);
+  }
+}
