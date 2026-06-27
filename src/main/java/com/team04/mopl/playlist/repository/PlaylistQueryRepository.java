418cb24f0aa67a0ef9ea3941d04ef752c82c9898
JungH200000 <jhpaxk0604@gmail.com>
2026-06-28 00:35:53 +0900

    feat: 플레이리스트 목록 조회 관련 QueryDSL Repository 구현

---
+++ src/main/java/com/team04/mopl/playlist/repository/PlaylistQueryRepository.java
@@ -0,0 +1,9 @@
+package com.team04.mopl.playlist.repository;
+
+import com.team04.mopl.playlist.dto.request.PlaylistSearchRequest;
+import com.team04.mopl.playlist.dto.response.PlaylistCursorPage;
+
+public interface PlaylistQueryRepository {
+
+	PlaylistCursorPage findPlaylists(PlaylistSearchRequest request);
+}
