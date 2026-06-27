329f829f588949751ac791f6f57edce2095c6b28
JungH200000 <jhpaxk0604@gmail.com>
2026-06-28 00:32:01 +0900

    feat: 플레이리스트 커서 목록 조회 응답 DTO 구현

---
+++ src/main/java/com/team04/mopl/playlist/dto/response/CursorResponsePlaylistDto.java
@@ -0,0 +1,31 @@
+package com.team04.mopl.playlist.dto.response;
+
+import java.util.List;
+import java.util.UUID;
+
+import com.team04.mopl.common.enums.SortDirection;
+
+public record CursorResponsePlaylistDto(
+
+	// 데이터 목록
+	List<PlaylistDto> data,
+
+	// 다음 커서
+	String nextCursor,
+
+	// 다음 요청의 보조 커서
+	UUID nextIdAfter,
+
+	// 다음 데이터가 있는지 여부
+	Boolean hasNext,
+
+	// 총 데이터 개수
+	Long totalCount,
+
+	// 정렬 기준
+	String sortBy,
+
+	// 정렬 방향
+	SortDirection sortDirection
+) {
+}
