b40d81e2b2baafae9969ea1a3d09fb93d9b29786
LeeDyol <dlekthf819203@naver.com>
2026-06-29 15:53:03 +0900

    refactor: DirectMessageDto 내 엔티티 직접 노출 제거 및 DTO 계층화

--- src/main/java/com/team04/mopl/directmessage/dto/response/DirectMessageDto.java
+++ src/main/java/com/team04/mopl/directmessage/dto/response/DirectMessageDto.java
@@ -3,7 +3,7 @@ package com.team04.mopl.directmessage.dto.response;
 import java.time.Instant;
 import java.util.UUID;
 
-import com.team04.mopl.user.entity.User;
+import com.team04.mopl.common.dto.UserSummary;
 
 import lombok.Builder;
 
@@ -12,8 +12,8 @@ public record DirectMessageDto(
 	UUID id,
 	UUID conversationId,
 	Instant createdAt,
-	User sender,
-	User receiver,
+	UserSummary sender,
+	UserSummary receiver,
 	String content
 ) {
 }
