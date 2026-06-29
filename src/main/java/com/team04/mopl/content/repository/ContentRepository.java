f4425a64f42fa8c223882770d3827c04872a34db
jsj <sungju3115@naver.com>
2026-06-29 10:22:15 +0900

    feat: 콘텐츠 목록 조회 - Repository 구현

--- src/main/java/com/team04/mopl/content/repository/ContentRepository.java
+++ src/main/java/com/team04/mopl/content/repository/ContentRepository.java
@@ -7,8 +7,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.team04.mopl.content.entity.CollectionSource;
 import com.team04.mopl.content.entity.Content;
+import com.team04.mopl.content.repository.qdsl.ContentQdslRepository;
 
-public interface ContentRepository extends JpaRepository<Content, UUID> {
+public interface ContentRepository extends JpaRepository<Content, UUID>, ContentQdslRepository {
 
 	/**
 	 * externalId + source 조합으로 중복 수집 여부 확인
