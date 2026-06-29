a430a35a13c44363ac9cf4bb27c1682d132051a0
parkngg <millco0803com@gmail.com>
2026-06-29 16:25:56 +0900

    feat: 사용자 미존재 에러 코드 추가

--- src/main/java/com/team04/mopl/user/exception/UserErrorCode.java
+++ src/main/java/com/team04/mopl/user/exception/UserErrorCode.java
@@ -15,8 +15,8 @@ public enum UserErrorCode implements ErrorCode {
 	ROLE_REQUIRED(HttpStatus.BAD_REQUEST, "US05", "권한은 필수입니다."),
 	SOCIAL_ACCOUNT_USER_REQUIRED(HttpStatus.BAD_REQUEST, "US06", "사용자는 필수입니다."),
 	SOCIAL_PROVIDER_REQUIRED(HttpStatus.BAD_REQUEST, "US07", "소셜 제공자는 필수입니다."),
-	SOCIAL_PROVIDER_USER_ID_REQUIRED(HttpStatus.BAD_REQUEST, "US08", "소셜 사용자 ID는 필수입니다.");
-
+	SOCIAL_PROVIDER_USER_ID_REQUIRED(HttpStatus.BAD_REQUEST, "US08", "소셜 사용자 ID는 필수입니다."),
+	USER_NOT_FOUND(HttpStatus.NOT_FOUND, "US09", "사용자를 찾을 수 없습니다.");
 
 	private final HttpStatus httpStatus;
 	private final String code;
