71f5dc7e71080d8ec7d79e3abc4743acbdcf4345
JungH200000 <jhpaxk0604@gmail.com>
2026-06-29 16:27:07 +0900

    feat: 플레이리스트 구독 취소 관련 커스텀 예외 코드 추가

--- src/main/java/com/team04/mopl/playlist/exception/PlaylistErrorCode.java
+++ src/main/java/com/team04/mopl/playlist/exception/PlaylistErrorCode.java
@@ -13,9 +13,10 @@ public enum PlaylistErrorCode implements ErrorCode {
 	NO_CHANGE_VALUE(HttpStatus.BAD_REQUEST, "PL02", "변경사항이 없습니다."),
 	PLAYLIST_FORBIDDEN(HttpStatus.FORBIDDEN, "PL03", "플레이리스트 소유자가 아닙니다."),
 	PLAYLIST_NOT_FOUND(HttpStatus.NOT_FOUND, "PL04", "플레이리스트를 찾을 수 없습니다."),
-	PLAYLIST_ALREADY_SUBSCRIBED(HttpStatus.CONFLICT, "PL05", "이미 구독한 플레이리스트입니다."),
-	PLAYLIST_HARD_DELETE_BATCH_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "PL06", "플레이리스트 물리 삭제 배치를 실패했습니다."),
-	PLAYLIST_SELF_SUBSCRIPTION_NOT_ALLOWED(HttpStatus.BAD_REQUEST, "PL07", "본인이 생성한 플레이리스트는 구독할 수 없습니다.");
+	PLAYLIST_UNSUBSCRIBED(HttpStatus.BAD_REQUEST, "PL05", "구독하지 않은 플레이리스트입니다."),
+	PLAYLIST_ALREADY_SUBSCRIBED(HttpStatus.CONFLICT, "PL06", "이미 구독한 플레이리스트입니다."),
+	PLAYLIST_SELF_SUBSCRIPTION_NOT_ALLOWED(HttpStatus.BAD_REQUEST, "PL07", "본인이 생성한 플레이리스트는 구독할 수 없습니다."),
+	PLAYLIST_SELF_UNSUBSCRIPTION_NOT_ALLOWED(HttpStatus.BAD_REQUEST, "PL08", "본인이 생성한 플레이리스트는 구독 취소 할 수 없습니다.");
 
 	private final HttpStatus httpStatus;
 	private final String code;
