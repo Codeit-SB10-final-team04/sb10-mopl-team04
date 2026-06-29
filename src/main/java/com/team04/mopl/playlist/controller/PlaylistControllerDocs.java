9205946fd56730f39a5d9c7b4ac19afcb20a97e6
JungH200000 <jhpaxk0604@gmail.com>
2026-06-29 14:16:07 +0900

    refactor: Security 추가로 인해 `UserDetails`를 사용하도록 리팩터링

--- src/main/java/com/team04/mopl/playlist/controller/PlaylistControllerDocs.java
+++ src/main/java/com/team04/mopl/playlist/controller/PlaylistControllerDocs.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
 
+import com.team04.mopl.auth.security.MoplUserDetails;
 import com.team04.mopl.playlist.dto.request.PlaylistCreateRequest;
 import com.team04.mopl.playlist.dto.request.PlaylistSearchRequest;
 import com.team04.mopl.playlist.dto.request.PlaylistUpdateRequest;
@@ -19,36 +20,31 @@ public interface PlaylistControllerDocs {
 	@Operation(summary = "플레이리스트 등록", description = "생성한 플레이리스트는 API 요청자 본인의 플레이리스트로 생성됩니다.")
 	ResponseEntity<PlaylistDto> createPlaylist(
 		PlaylistCreateRequest request,
-		UUID currentUserId
-		// MoplUserDetails moplUserDetails
+		MoplUserDetails moplUserDetails
 	);
 
 	@Operation(summary = "플레이리스트 단건 조회")
 	ResponseEntity<PlaylistDto> findPlaylist(
 		UUID playlistId,
-		UUID currentUserId
-		// MoplUserDetails moplUserDetails
+		MoplUserDetails moplUserDetails
 	);
 
 	@Operation(summary = "플레이리스트 목록 조회 (커서 페이지네이션)")
 	ResponseEntity<CursorResponsePlaylistDto> findAllPlaylists(
 		PlaylistSearchRequest request,
-		UUID currentUserId
-		// MoplUserDetails moplUserDetails
+		MoplUserDetails moplUserDetails
 	);
 
 	@Operation(summary = "플레이리스트 수정", description = "플레이리스트 소유자만 수정할 수 있습니다.")
 	ResponseEntity<PlaylistDto> updatePlaylist(
 		UUID playlistId,
 		PlaylistUpdateRequest request,
-		UUID currentUserId
-		// MoplUserDetails moplUserDetails
+		MoplUserDetails moplUserDetails
 	);
 
 	@Operation(summary = "플레이리스트 삭제", description = "플레이리스트 소유자만 삭제할 수 있습니다.")
 	ResponseEntity<Void> softDeletePlaylist(
 		UUID playlistId,
-		UUID currentUserId
-		// MoplUserDetails moplUserDetails
+		MoplUserDetails moplUserDetails
 	);
 }
