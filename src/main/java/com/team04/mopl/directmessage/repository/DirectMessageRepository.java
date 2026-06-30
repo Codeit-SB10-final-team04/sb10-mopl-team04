56ca6b2ddea3b10a16d721bb100bd0deca2310f9
LeeDyol <dlekthf819203@naver.com>
2026-06-30 13:45:51 +0900

    fix: 최근 메시지 조회 시 보조 정렬 조건(ID) 추가하여 결과 정합성 보장

--- src/main/java/com/team04/mopl/directmessage/repository/DirectMessageRepository.java
+++ src/main/java/com/team04/mopl/directmessage/repository/DirectMessageRepository.java
@@ -10,7 +10,7 @@ import com.team04.mopl.directmessage.entity.DirectMessage;
 public interface DirectMessageRepository extends JpaRepository<DirectMessage, UUID> {
 
 	// 최근 메시지 단건 조회
-	Optional<DirectMessage> findTopByConversationIdOrderByCreatedAtDesc(UUID conversationId);
+	Optional<DirectMessage> findTopByConversationIdOrderByCreatedAtDescIdDesc(UUID conversationId);
 
 	// 안 읽음 메시지 존재 유무
 	boolean existsByConversationIdAndReceiverIdAndReadFalse(UUID conversationId, UUID receiverId);
