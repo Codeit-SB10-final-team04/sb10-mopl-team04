35e0801c9e5617a3abd90c9ab66b41563da51d7f
JungH200000 <jhpaxk0604@gmail.com>
2026-06-30 15:29:11 +0900

    feat: 알림 생성에 관련된 커스텀 예외/예외 코드 구현

---
+++ src/main/java/com/team04/mopl/notification/exception/NotificationErrorCode.java
@@ -0,0 +1,32 @@
+package com.team04.mopl.notification.exception;
+
+import org.springframework.http.HttpStatus;
+
+import com.team04.mopl.common.exception.ErrorCode;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public enum NotificationErrorCode implements ErrorCode {
+
+	NOTIFICATION_INVALID_INPUT(HttpStatus.BAD_REQUEST, "NO01", "잘못된 입력값입니다.");
+
+	private final HttpStatus httpStatus;
+	private final String code;
+	private final String description;
+
+	@Override
+	public HttpStatus getHttpStatus() {
+		return this.httpStatus;
+	}
+
+	@Override
+	public String getCode() {
+		return this.code;
+	}
+
+	@Override
+	public String getMessage() {
+		return this.description;
+	}
+}
