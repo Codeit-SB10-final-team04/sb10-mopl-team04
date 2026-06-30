ef9335818ec6874aa804bd3968e2a034dfbb5a2f
jsj <sungju3115@naver.com>
2026-06-30 11:41:36 +0900

    fix: dto 검증 조건 추가

--- src/main/java/com/team04/mopl/content/dto/request/ContentUpdateRequest.java
+++ src/main/java/com/team04/mopl/content/dto/request/ContentUpdateRequest.java
@@ -2,8 +2,13 @@ package com.team04.mopl.content.dto.request;
 
 import java.util.List;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record ContentUpdateRequest(
+	@NotBlank(message = "제목은 공백일 수 없습니다.")
 	String title,
+
+	@NotBlank(message = "설명은 공백일 수 없습니다.")
 	String description,
 	List<String> tags
 ) {
