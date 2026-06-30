5b469226fd5c229b628f1ef07f6aa3735bff4bea
jsj <sungju3115@naver.com>
2026-07-01 08:02:53 +0900

    feat: ReviewRepository 구현

---
+++ src/main/java/com/team04/mopl/review/repository/ReviewRepository.java
@@ -0,0 +1,10 @@
+package com.team04.mopl.review.repository;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.team04.mopl.review.entity.Review;
+
+public interface ReviewRepository extends JpaRepository<Review, UUID> {
+}
