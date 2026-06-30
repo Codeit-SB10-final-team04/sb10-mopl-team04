b8fa0572afb6a54c9a51540844b82eb9dbe9a7d5
jsj <sungju3115@naver.com>
2026-07-01 08:03:28 +0900

    feat: 리뷰 등록 ControllerAPI 구현

---
+++ src/main/java/com/team04/mopl/review/controller/ReviewController.java
@@ -0,0 +1,33 @@
+package com.team04.mopl.review.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.team04.mopl.auth.security.MoplUserDetails;
+import com.team04.mopl.review.dto.requeset.ReviewCreateRequest;
+import com.team04.mopl.review.dto.response.ReviewDto;
+import com.team04.mopl.review.service.ReviewService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/reviews")
+@RequiredArgsConstructor
+public class ReviewController implements ReviewControllerDocs {
+
+	private final ReviewService reviewService;
+
+	@Override
+	@PostMapping
+	public ResponseEntity<ReviewDto> createReview(@RequestBody ReviewCreateRequest reviewCreateRequest,
+		@AuthenticationPrincipal MoplUserDetails moplUserDetails) {
+
+		ReviewDto reviewDto = reviewService.createReview(reviewCreateRequest);
+
+		return ResponseEntity.ok(reviewDto);
+	}
+}
---
+++ src/main/java/com/team04/mopl/review/controller/ReviewControllerDocs.java
@@ -0,0 +1,16 @@
+package com.team04.mopl.review.controller;
+
+import org.springframework.http.ResponseEntity;
+
+import com.team04.mopl.auth.security.MoplUserDetails;
+import com.team04.mopl.review.dto.requeset.ReviewCreateRequest;
+import com.team04.mopl.review.dto.response.ReviewDto;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "리뷰 API", description = "리뷰 관리")
+public interface ReviewControllerDocs {
+	@Operation
+	ResponseEntity<ReviewDto> createReview(ReviewCreateRequest reviewCreateRequest, MoplUserDetails moplUserDetails);
+}
