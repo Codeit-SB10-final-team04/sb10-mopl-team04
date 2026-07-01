6377c10d005c0ee49973b71acac4c1a616ffaebe
jsj <sungju3115@naver.com>
2026-07-01 16:34:17 +0900

    fix: dto 검증 조건 추가

--- src/main/java/com/team04/mopl/review/dto/request/ReviewUpdateRequest.java
+++ src/main/java/com/team04/mopl/review/dto/request/ReviewUpdateRequest.java
@@ -1,7 +1,14 @@
 package com.team04.mopl.review.dto.request;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+
 public record ReviewUpdateRequest(
+	@NotBlank
 	String text,
+
+	@Min(1) @Max(5)
 	Short rating
 ) {
 }
