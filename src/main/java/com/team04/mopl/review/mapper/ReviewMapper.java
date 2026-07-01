80180df80664f9dcd9e56b15b3c53402ddbcba20
jsj <sungju3115@naver.com>
2026-07-01 10:07:44 +0900

    feat: ReviewMapper 구현

---
+++ src/main/java/com/team04/mopl/review/mapper/ReviewMapper.java
@@ -0,0 +1,14 @@
+package com.team04.mopl.review.mapper;
+
+import org.mapstruct.Mapper;
+
+import com.team04.mopl.common.dto.UserSummary;
+import com.team04.mopl.config.MapStructConfig;
+import com.team04.mopl.review.dto.response.ReviewDto;
+import com.team04.mopl.review.entity.Review;
+
+@Mapper(config = MapStructConfig.class)
+public interface ReviewMapper {
+
+	ReviewDto toDto(Review review, UserSummary author);
+}
