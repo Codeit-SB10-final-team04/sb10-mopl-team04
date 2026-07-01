6ee9700fb3481227f294d46c46bdd263ac50430f
jsj <sungju3115@naver.com>
2026-07-01 10:07:11 +0900

    feat: Repository 집계 함수 && 존재 여부 검증 메서드 추가 구현

--- src/main/java/com/team04/mopl/review/repository/ReviewRepository.java
+++ src/main/java/com/team04/mopl/review/repository/ReviewRepository.java
@@ -7,4 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.team04.mopl.review.entity.Review;
 
 public interface ReviewRepository extends JpaRepository<Review, UUID> {
+
+	boolean existsByUserIdAndContentIdAndDeletedAtIsNull(UUID userId, UUID contentId);
 }
