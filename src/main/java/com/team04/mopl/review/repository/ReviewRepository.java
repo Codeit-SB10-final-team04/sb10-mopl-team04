274917a1ca5948e43446ce99e1eb88064351c30e
jsj <sungju3115@naver.com>
2026-07-01 13:11:25 +0900

    style: 코드 스타일 위배로 줄바꿈 추가

--- src/main/java/com/team04/mopl/review/repository/ReviewRepository.java
+++ src/main/java/com/team04/mopl/review/repository/ReviewRepository.java
@@ -10,6 +10,14 @@ import com.team04.mopl.review.entity.Review;
 
 public interface ReviewRepository extends JpaRepository<Review, UUID> {
 
-	@Query(value = "SELECT EXISTS(SELECT 1 FROM content_reviews WHERE user_id = :userId AND content_id = :contentId AND deleted_at IS NULL)", nativeQuery = true)
-	boolean existsByUserIdAndContentIdAndDeletedAtIsNull(@Param("userId") UUID userId, @Param("contentId") UUID contentId);
+	@Query(value = """
+		SELECT EXISTS(
+		    SELECT 1 FROM content_reviews
+		    WHERE user_id = :userId
+		      AND content_id = :contentId
+		      AND deleted_at IS NULL
+		)
+		""", nativeQuery = true)
+	boolean existsByUserIdAndContentIdAndDeletedAtIsNull(@Param("userId") UUID userId,
+		@Param("contentId") UUID contentId);
 }
