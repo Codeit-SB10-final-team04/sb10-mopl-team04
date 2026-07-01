f0ead6e6d665f973e7e005e017ed1e96c872e618
jsj <sungju3115@naver.com>
2026-07-01 13:03:57 +0900

    style: import문 정리

--- src/main/java/com/team04/mopl/review/controller/ReviewController.java
+++ src/main/java/com/team04/mopl/review/controller/ReviewController.java
@@ -4,7 +4,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +12,7 @@ import com.team04.mopl.review.dto.requeset.ReviewCreateRequest;
 import com.team04.mopl.review.dto.response.ReviewDto;
 import com.team04.mopl.review.service.ReviewService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
