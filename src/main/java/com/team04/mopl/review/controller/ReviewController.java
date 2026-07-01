178b9731156f7b9183fdecaa706be40c82a9dadd
jsj <sungju3115@naver.com>
2026-07-01 14:35:00 +0900

    feat: 리뷰 수정 Controller API 구현

--- src/main/java/com/team04/mopl/review/controller/ReviewController.java
+++ src/main/java/com/team04/mopl/review/controller/ReviewController.java
@@ -1,8 +1,12 @@
 package com.team04.mopl.review.controller;
 
+import java.util.UUID;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.team04.mopl.auth.security.MoplUserDetails;
 import com.team04.mopl.review.dto.request.ReviewCreateRequest;
+import com.team04.mopl.review.dto.request.ReviewUpdateRequest;
 import com.team04.mopl.review.dto.response.ReviewDto;
 import com.team04.mopl.review.service.ReviewService;
 
@@ -29,6 +34,19 @@ public class ReviewController implements ReviewControllerDocs {
 		@AuthenticationPrincipal MoplUserDetails moplUserDetails) {
 
 		ReviewDto reviewDto = reviewService.createReview(reviewCreateRequest, moplUserDetails);
+
+		return ResponseEntity.status(HttpStatus.OK).body(reviewDto);
+	}
+
+	@Override
+	@PatchMapping("/{reviewId}")
+	public ResponseEntity<ReviewDto> updateReview(
+		@PathVariable UUID reviewId,
+		@Valid @RequestBody ReviewUpdateRequest reviewUpdateRequest,
+		@AuthenticationPrincipal MoplUserDetails moplUserDetails
+	) {
+
+		ReviewDto reviewDto;
 
 		return ResponseEntity.status(HttpStatus.OK).body(reviewDto);
 	}
