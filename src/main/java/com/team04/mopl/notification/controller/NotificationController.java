7de44345b594821b48fea98faaeb7e1011e70912
JungH200000 <jhpaxk0604@gmail.com>
2026-07-01 16:08:24 +0900

    feat: 알림 읽음 Controller 로직 구현

--- src/main/java/com/team04/mopl/notification/controller/NotificationController.java
+++ src/main/java/com/team04/mopl/notification/controller/NotificationController.java
@@ -5,8 +5,10 @@ import java.util.UUID;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,5 +38,17 @@ public class NotificationController implements NotificationControllerDocs {
 			notificationService.findAllNotifications(request, currentUserId);
 
 		return ResponseEntity.status(HttpStatus.OK).body(cursorResponseNotificationDto);
+	}
+
+	@DeleteMapping(value = "/{notificationId}")
+	@Override
+	public ResponseEntity<Void> readNotification(
+		@PathVariable UUID notificationId,
+		@AuthenticationPrincipal MoplUserDetails moplUserDetails
+	) {
+		UUID currentUserId = moplUserDetails.getUserId();
+		notificationService.readNotification(notificationId, currentUserId);
+
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
 	}
 }
--- src/main/java/com/team04/mopl/notification/controller/NotificationControllerDocs.java
+++ src/main/java/com/team04/mopl/notification/controller/NotificationControllerDocs.java
@@ -1,5 +1,7 @@
 package com.team04.mopl.notification.controller;
 
+import java.util.UUID;
+
 import org.springframework.http.ResponseEntity;
 
 import com.team04.mopl.auth.security.MoplUserDetails;
@@ -13,6 +15,12 @@ public interface NotificationControllerDocs {
 	@Operation(summary = "알림 목록 조회 (커서 페이지네이션)", description = "API 요청자의 알림 목록만 조회할 수 있습니다.")
 	ResponseEntity<CursorResponseNotificationDto> findAllNotifications(
 		NotificationSearchRequest request,
+		MoplUserDetails moplUserDetails
+	);
+
+	@Operation(summary = "알림 읽음 처리")
+	ResponseEntity<Void> readNotification(
+		UUID notificationId,
 		MoplUserDetails moplUserDetails
 	);
 }
