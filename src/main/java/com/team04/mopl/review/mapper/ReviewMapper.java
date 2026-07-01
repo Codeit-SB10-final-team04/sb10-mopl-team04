619d31bddc85a18881c21cd70d5ede9f60ede65e
jsj <sungju3115@naver.com>
2026-07-01 10:12:20 +0900

    feat: Mapper 커버리지 측정 데외

--- src/main/java/com/team04/mopl/review/mapper/ReviewMapper.java
+++ src/main/java/com/team04/mopl/review/mapper/ReviewMapper.java
@@ -1,5 +1,7 @@
 package com.team04.mopl.review.mapper;
 
+import javax.annotation.processing.Generated;
+
 import org.mapstruct.Mapper;
 
 import com.team04.mopl.common.dto.UserSummary;
@@ -8,6 +10,7 @@ import com.team04.mopl.review.dto.response.ReviewDto;
 import com.team04.mopl.review.entity.Review;
 
 @Mapper(config = MapStructConfig.class)
+@Generated("jacoco-exclude")
 public interface ReviewMapper {
 
 	ReviewDto toDto(Review review, UserSummary author);
