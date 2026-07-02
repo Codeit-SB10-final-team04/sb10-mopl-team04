0f2a8bd801aabad6936477e03b2f0c5e3f61d1a9
JungH200000 <jhpaxk0604@gmail.com>
2026-07-02 14:50:59 +0900

    feat: Kafka topic 상수 클래스 구현

---
+++ src/main/java/com/team04/mopl/notification/kafka/NotificationKafkaTopics.java
@@ -0,0 +1,10 @@
+package com.team04.mopl.notification.kafka;
+
+// topic 상수만 모아두는 클래스
+public final class NotificationKafkaTopics {
+
+	public static final String PLAYLIST_SUBSCRIBED = "mopl.playlist.subscribed";
+
+	private NotificationKafkaTopics() {
+	}
+}
