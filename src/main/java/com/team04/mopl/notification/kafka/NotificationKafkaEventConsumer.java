aa5b7f7dfbc7db236016351108f0cf05dba10264
JungH200000 <jhpaxk0604@gmail.com>
2026-07-02 14:52:26 +0900

    feat: Kafka topic을 구독해 알림 저장/전송하는 listener 구현

---
+++ src/main/java/com/team04/mopl/notification/kafka/NotificationKafkaEventConsumer.java
@@ -0,0 +1,91 @@
+package com.team04.mopl.notification.kafka;
+
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.team04.mopl.notification.dto.response.NotificationDto;
+import com.team04.mopl.notification.enums.NotificationLevel;
+import com.team04.mopl.notification.enums.NotificationType;
+import com.team04.mopl.notification.kafka.exception.KafkaEventErrorCode;
+import com.team04.mopl.notification.kafka.exception.KafkaEventException;
+import com.team04.mopl.notification.service.NotificationService;
+import com.team04.mopl.playlist.event.PlaylistSubscribedEvent;
+import com.team04.mopl.playlist.repository.PlaylistRepository;
+import com.team04.mopl.user.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+// Kafka topic을 구독해서 알림 저장/전송하는 listener
+// TODO: SSE를 이용해 서버에서 실시간 알림 전송 구현 예정
+// TODO: 지금은 DB에 저장까지만
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class NotificationKafkaEventConsumer {
+
+	private final UserRepository userRepository;
+	private final PlaylistRepository playlistRepository;
+	private final NotificationService notificationService;
+
+	private final ObjectMapper objectMapper;
+
+	// 특정 사용자가 플레이리스트 구독 시 플레이리스트 소유주에게 알림을 보내는 listener
+	@KafkaListener(topics = NotificationKafkaTopics.PLAYLIST_SUBSCRIBED)
+	public void consumePlaylistSubscribedEvent(String kafkaEvent) {
+		PlaylistSubscribedEvent event = deserialize(kafkaEvent, PlaylistSubscribedEvent.class);
+
+		// title
+		String title = "새 플레이리스트 구독 알림";
+
+		// content
+		String content = String.format("[%s] 님이 [%s] 플레이리스트를 구독했습니다.",
+			event.subscriberName(),
+			event.playlistTitle()
+		);
+
+		// 알림 저장 및 TODO: SSE 전송
+		sendNotification(
+			Set.of(event.playlistOwnerId()),
+			title,
+			content,
+			NotificationType.SUBSCRIBE,
+			NotificationLevel.INFO
+		);
+	}
+
+	// KafkaEvent 역직렬화
+	private <T> T deserialize(String kafkaEvent, Class<T> eventClass) {
+		try {
+			return objectMapper.readValue(kafkaEvent, eventClass);
+		} catch (JsonProcessingException e) {
+			log.error("[EVENT_DESERIALIZATION_FAILED] 이벤트 역직렬화 실패", e);
+			throw new KafkaEventException(KafkaEventErrorCode.KAFKA_EVENT_DESERIALIZATION_FAILED)
+				.addDetail("event", eventClass.getSimpleName());
+		}
+	}
+
+	private void sendNotification(
+		Set<UUID> receiverIds,
+		String title,
+		String content,
+		NotificationType type,
+		NotificationLevel level
+	) {
+		List<NotificationDto> notificationDtoList = notificationService.saveNotificationList(
+			receiverIds,
+			title,
+			content,
+			type,
+			level
+		);
+
+		// TODO: SSE로 알림 전송
+	}
+}
