280425cfdcb69e6b009989c1785cd15e94f7d88b
JungH200000 <jhpaxk0604@gmail.com>
2026-07-02 16:20:13 +0900

    feat: 구독 중인 플레이리스트에 콘텐츠가 추가되면 해당 플레이리스트 구독자에게 발행하는 이벤트 구현

---
+++ src/main/java/com/team04/mopl/playlist/event/PlaylistContentAddEvent.java
@@ -0,0 +1,39 @@
+package com.team04.mopl.playlist.event;
+
+import java.time.Instant;
+import java.util.UUID;
+
+// 구독 중인 플레이리스트에 콘텐츠가 추가되면 해당 플레이리스트 구독자에게 발행하는 이벤트
+public record PlaylistContentAddEvent(
+
+	UUID eventId,
+
+	UUID playlistId,
+	String playlistTitle,
+
+	UUID playlistOwnerId,
+
+	UUID contentId,
+	String contentTitle,
+
+	// 이벤트 발생 시간
+	Instant occurredAt
+) {
+	public static PlaylistContentAddEvent of(
+		UUID playlistId,
+		String playlistTitle,
+		UUID playlistOwnerId,
+		UUID playlistContentId,
+		String playlistContentTitle
+	) {
+		return new PlaylistContentAddEvent(
+			UUID.randomUUID(),
+			playlistId,
+			playlistTitle,
+			playlistOwnerId,
+			playlistContentId,
+			playlistContentTitle,
+			Instant.now()
+		);
+	}
+}
