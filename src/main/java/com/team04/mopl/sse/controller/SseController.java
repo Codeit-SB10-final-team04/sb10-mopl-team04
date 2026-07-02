832fbed77370f9d8313384dcaa61e62bcf72c0c7
JungH200000 <jhpaxk0604@gmail.com>
2026-07-03 00:27:00 +0900

    refactor: `SseController` 클래스를 `sse` 패키지 하위로 이동

--- src/main/java/com/team04/mopl/sse/controller/SseController.java
+++ src/main/java/com/team04/mopl/sse/controller/SseController.java
@@ -1,4 +1,4 @@
-package com.team04.mopl.notification.controller;
+package com.team04.mopl.sse.controller;
 
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
