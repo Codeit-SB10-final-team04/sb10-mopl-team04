77eefdeddd7b60d35331071558c77cfca815fffd
jsj <sungju3115@naver.com>
2026-07-03 17:08:11 +0900

    fix: 콘텐츠 실시간 채팅 Dto 오타 및 이름 수정

---
+++ src/main/java/com/team04/mopl/content/dto/request/ContentChatSendRequest.java
@@ -0,0 +1,4 @@
+package com.team04.mopl.content.dto.request;
+
+public record ContentChatSendRequest(String content) {
+}
--- src/main/java/com/team04/mopl/content/dto/request/ContentSendRequest.java
+++
@@ -1,6 +0,0 @@
-package com.team04.mopl.content.dto.request;
-
-import com.team04.mopl.common.dto.UserSummary;
-
-public record ContentSendRequest(UserSummary sender, String content) {
-}
