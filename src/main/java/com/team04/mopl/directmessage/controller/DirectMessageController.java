e3b75a54b2c9a75f8535be7647fb5d7c4000eb0a
LeeDyol <dlekthf819203@naver.com>
2026-07-03 19:41:02 +0900

    feat: DM 목록 조회 API 컨트롤러 구현

--- src/main/java/com/team04/mopl/directmessage/controller/DirectMessageController.java
+++ src/main/java/com/team04/mopl/directmessage/controller/DirectMessageController.java
@@ -5,12 +5,16 @@ import java.util.UUID;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.team04.mopl.auth.security.MoplUserDetails;
+import com.team04.mopl.directmessage.dto.request.DirectMessagePagedRequest;
+import com.team04.mopl.directmessage.dto.response.CursorResponseDirectMessageDto;
 import com.team04.mopl.directmessage.service.DirectMessageService;
 
 import lombok.RequiredArgsConstructor;
@@ -33,5 +37,22 @@ public class DirectMessageController implements DirectMessageControllerDocs {
 		directMessageService.markAsRead(conversationId, directMessageId, moplUserDetails.getUserId());
 
 		return ResponseEntity.status(HttpStatus.OK).build();
+	}
+
+	@Override
+	@GetMapping("/{conversationId}/direct-messages")
+	public ResponseEntity<CursorResponseDirectMessageDto> findAll(
+		@PathVariable UUID conversationId,
+		@ModelAttribute DirectMessagePagedRequest directMessagePagedRequest,
+		@AuthenticationPrincipal MoplUserDetails moplUserDetails
+	) {
+
+		CursorResponseDirectMessageDto cursorResponseDirectMessageDto = directMessageService.findAll(
+			conversationId,
+			directMessagePagedRequest,
+			moplUserDetails.getUserId()
+		);
+
+		return ResponseEntity.status(HttpStatus.OK).body(cursorResponseDirectMessageDto);
 	}
 }
