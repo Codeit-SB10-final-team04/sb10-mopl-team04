e21e76859bb90bbad2c04a92611c76d12af81e72
JungH200000 <jhpaxk0604@gmail.com>
2026-07-04 14:54:47 +0900

    feat: 사용자 권한 변경 topic 추가

--- src/main/java/com/team04/mopl/notification/kafka/NotificationKafkaTopics.java
+++ src/main/java/com/team04/mopl/notification/kafka/NotificationKafkaTopics.java
@@ -7,6 +7,7 @@ public final class NotificationKafkaTopics {
 	public static final String PLAYLIST_CONTENT_ADDED = "mopl.playlist.content.added";
 	public static final String FOLLOW_CREATED = "mopl.follow.created";
 	public static final String PLAYLIST_CREATED = "mopl.playlist.created";
+	public static final String USER_ROLE_CHANGED = "mopl.user.role.changed";
 
 	private NotificationKafkaTopics() {
 	}
