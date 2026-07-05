b074e902f47f48221419440cc2de9efdbaa1b2d0
JungH200000 <jhpaxk0604@gmail.com>
2026-07-05 21:54:42 +0900

    chore: Swagger 문서 `@Operation` 작성 및 `@Tag` 수정

--- src/main/java/com/team04/mopl/sse/controller/SseControllerDocs.java
+++ src/main/java/com/team04/mopl/sse/controller/SseControllerDocs.java
@@ -6,11 +6,13 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import com.team04.mopl.auth.security.MoplUserDetails;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
-@Tag(name = "sse-controller")
+@Tag(name = "SSE")
 public interface SseControllerDocs {
 
+	@Operation(summary = "SSE 연결", description = "lastEventId 이후 발생한 미수신 알림을 복구하여 SSE로 전송합니다.")
 	SseEmitter connect(
 		MoplUserDetails moplUserDetails,
 		UUID lastEventId
