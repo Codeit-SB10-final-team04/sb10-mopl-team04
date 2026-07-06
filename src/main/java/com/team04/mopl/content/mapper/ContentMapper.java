1156d876e50c57181839fda6e231061845677aea
jsj <sungju3115@naver.com>
2026-07-06 10:31:13 +0900

    fix: MapStruct ContentMapper watcherCount 매핑 인메모리 파라미터로 수정

--- src/main/java/com/team04/mopl/content/mapper/ContentMapper.java
+++ src/main/java/com/team04/mopl/content/mapper/ContentMapper.java
@@ -3,6 +3,7 @@ package com.team04.mopl.content.mapper;
 import java.util.List;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import com.team04.mopl.config.MapStructConfig;
 import com.team04.mopl.content.dto.response.ContentDto;
@@ -11,5 +12,7 @@ import com.team04.mopl.content.entity.Content;
 @Mapper(config = MapStructConfig.class)
 public interface ContentMapper {
 
+	// Content 엔티티의 watcherCount가 아닌 파라미터의 watcherCount를 사용
+	@Mapping(source = "watcherCount", target = "watcherCount")
 	ContentDto toDto(Content content, List<String> tags, Long watcherCount);
 }
