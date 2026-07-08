c0de54e7e1d84e46624009162c9b4c6274178ce1
JungH200000 <jhpaxk0604@gmail.com>
2026-07-08 11:28:05 +0900

    feat: SSE 이벤트 이름을 관리하는 상수 클래스 `SseEventNames` 구현

---
+++ src/main/java/com/team04/mopl/sse/event/SseEventNames.java
@@ -0,0 +1,12 @@
+package com.team04.mopl.sse.event;
+
+// SSE 이벤트 이름을 관리하는 상수 클래스
+// 클라이언트가 구독하는 이벤트 이름과 서버에서 전송하는 이벤트 이름이 일치해야 하기에 문자열이 아닌 상수로 관리
+// 한 번 초기화한 값을 변경하지 않게 하기 위해 final 사용
+public final class SseEventNames {
+
+	public static final String NOTIFICATIONS = "notifications";
+
+	private SseEventNames() {
+	}
+}
