61234c5413b6ac404fe40060e3db49040918aec6
JungH200000 <jhpaxk0604@gmail.com>
2026-07-08 11:48:42 +0900

    refactor: 이벤트 이름을 `SseEventNames`를 사용하도록 수정

---
+++ src/main/java/com/team04/mopl/notification/realtime/NotificationRealtimePublisher.java
@@ -0,0 +1,11 @@
+package com.team04.mopl.notification.realtime;
+
+import com.team04.mopl.notification.dto.response.NotificationDto;
+
+// 알림 실시간 전송 요청을 추상화한 인터페이스
+// 알림 저장 이후 실시간 전송 로직을 Consumer로 분리
+// 로컬 SSE 전송이나 Redis Pub/Sub 기반 전송으로 교체 가능하게 함
+public interface NotificationRealtimePublisher {
+
+	void publish(NotificationDto notificationDto);
+}
