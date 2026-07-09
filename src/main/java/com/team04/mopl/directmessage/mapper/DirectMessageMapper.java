c5c0548a25e0784f487ed815a6f3e4191d7f4448
LeeDyol <dlekthf819203@naver.com>
2026-07-09 11:00:36 +0900

    feat: DirectMessageDto 리스트 변환 및 CursorResponseDirecteMessageDto 리스트 변환 로직 구현

--- src/main/java/com/team04/mopl/directmessage/mapper/DirectMessageMapper.java
+++ src/main/java/com/team04/mopl/directmessage/mapper/DirectMessageMapper.java
@@ -1,10 +1,14 @@
 package com.team04.mopl.directmessage.mapper;
 
+import java.util.List;
+import java.util.UUID;
+
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 import com.team04.mopl.common.dto.UserSummary;
 import com.team04.mopl.config.MapStructConfig;
+import com.team04.mopl.directmessage.dto.response.CursorResponseDirectMessageDto;
 import com.team04.mopl.directmessage.dto.response.DirectMessageDto;
 import com.team04.mopl.directmessage.entity.DirectMessage;
 import com.team04.mopl.user.entity.User;
@@ -17,6 +21,30 @@ public interface DirectMessageMapper {
 	@Mapping(target = "sender", expression = "java(toUserSummary(directMessage.getSender()))")
 	@Mapping(target = "receiver", expression = "java(toUserSummary(directMessage.getReceiver()))")
 	DirectMessageDto toDto(DirectMessage directMessage);
+
+	// DirectMessageList -> DirectMessageDto
+	List<DirectMessageDto> toDtoList(List<DirectMessage> directMessages);
+
+	// DirectMessageDto -> CursorResponseDirectMessageDto
+	default CursorResponseDirectMessageDto toCursorPageResponse(
+		List<DirectMessageDto> data,
+		String nextCursor,
+		UUID nextIdAfter,
+		boolean hasNext,
+		Long totalCount,
+		String sortBy,
+		String sortDirection
+	) {
+		return CursorResponseDirectMessageDto.builder()
+			.data(data)
+			.nextCursor(nextCursor)
+			.nextIdAfter(nextIdAfter)
+			.hasNext(hasNext)
+			.totalCount(totalCount)
+			.sortBy(sortBy)
+			.sortDirection(sortDirection)
+			.build();
+	}
 
 	// User -> UserSummary 변환
 	default UserSummary toUserSummary(User user) {
