838d0012b92b7c42aabe7f716384e88b9279144a
LeeDyol <dlekthf819203@naver.com>
2026-07-09 11:22:37 +0900

    feat: DM 목록 조회를 위한 QueryDSL 커서 페이지네이션 구현

---
+++ src/main/java/com/team04/mopl/directmessage/repository/qdsl/DirectMessageQdslRepository.java
@@ -0,0 +1,28 @@
+package com.team04.mopl.directmessage.repository.qdsl;
+
+import java.util.List;
+import java.util.UUID;
+
+import com.team04.mopl.directmessage.dto.request.DirectMessagePagedRequest;
+import com.team04.mopl.directmessage.entity.DirectMessage;
+
+/*
+  DirectMessageQdslRepository
+  ----------------------------
+  정렬 및 페이지네이션에 활용할 Query DSL 전용 인터페이스 선언
+ */
+public interface DirectMessageQdslRepository {
+	// 정렬 + 커서 페이지네이션이 적용된 DM 목록 조회
+	List<DirectMessage> findDirectMessageByCursor(
+		UUID conversationId,
+		DirectMessagePagedRequest directMessagePagedRequest,
+		UUID requestId
+	);
+
+	// DM 목록의 전체 개수 조회
+	Long countDirectMessage(
+		UUID conversationId,
+		DirectMessagePagedRequest directMessagePagedRequest,
+		UUID requestId
+	);
+}
