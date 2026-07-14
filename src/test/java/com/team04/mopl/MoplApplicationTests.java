9b8d96002031c21060ad533ee7d77edd8961fa28
LeeDyol <dlekthf819203@naver.com>
2026-07-14 12:24:58 +0900

    test: 통합 테스트 환경 내 Elasticsearch 빈 누락 이슈 해결

--- src/test/java/com/team04/mopl/MoplApplicationTests.java
+++ src/test/java/com/team04/mopl/MoplApplicationTests.java
@@ -2,12 +2,22 @@ package com.team04.mopl;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import com.team04.mopl.conversation.repository.es.ConversationElasticSearchRepository;
 
 @SpringBootTest
 class MoplApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@MockitoBean
+	private ConversationElasticSearchRepository conversationElasticSearchRepository;
+
+	@MockitoBean
+	private ElasticsearchOperations elasticsearchOperations;
+
+	@Test
+	void contextLoads() {
+	}
 
 }
