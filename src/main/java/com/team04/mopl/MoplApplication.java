5bcdd05f153f848c71d2fd466bef2a6d5a0cff3f
jsj <sungju3115@naver.com>
2026-07-15 13:59:38 +0900

    fix: Spring Boot ES 자동설정 제외(OpenSearch 충돌 방지)

--- src/main/java/com/team04/mopl/MoplApplication.java
+++ src/main/java/com/team04/mopl/MoplApplication.java
@@ -2,8 +2,17 @@ package com.team04.mopl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.elasticsearch.ElasticsearchClientAutoConfiguration;
+import org.springframework.boot.autoconfigure.elasticsearch.ReactiveElasticsearchClientAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(
+	exclude = {
+		ElasticsearchDataAutoConfiguration.class,
+		ElasticsearchClientAutoConfiguration.class,
+		ReactiveElasticsearchClientAutoConfiguration.class
+	}
+)
 public class MoplApplication {
 
     public static void main(String[] args) {
